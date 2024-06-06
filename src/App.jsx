b27910043c1384619475ad7704ee5e2f321ea7fb
Jesus Rodriguez <jesus.it.dev@gmail.com>
2024-06-06 00:01:22 -0600

    Added todos logic and delete button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,41 @@ import "./styles.css"
 
 export default function App() {
   const [newItem, setNewItem  ] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos((currentTodos) =>{
+      return [
+        ...currentTodos,{id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+
+    setNewIte("")
+  }
+
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo=>{
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
    <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
@@ -13,12 +45,19 @@ export default function App() {
       <button className="btn">Add</button>
     </form>
     <h1 className="header">Todo List</h1>
-    <ul>
-      <label>
-        <input type="checkbox" />
-        Item 1
-      </label>
-      <button className="btn btn-danger"></button>
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo =>{
+        return(
+          <li key={todo.id}>
+        <label>
+          <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+          {todo.title}
+        </label>
+        <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </li>
+        )
+      })} 
     </ul>
     </>
     ) 
